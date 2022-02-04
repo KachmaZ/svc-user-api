a3c197a3572f370adec9b830a3edd17814198de6
KachmaZ <boss.kachmaz@mail.ru>
2022-02-04 16:18:25 +0300

    Bootstrap connection

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Импортировать файлы CSS Bootstrap и Bootstrap Vue (порядок важен)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// Сделайте BootstrapVue доступным для всего проекта
+Vue.use(BootstrapVue)
+// При желании установите плагин компонентов иконок BootstrapVue
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
