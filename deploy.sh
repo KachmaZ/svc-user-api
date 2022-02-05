18a8c6b60361dc2276ffa773cdcd3eacce44806d
KachmaZ <boss.kachmaz@mail.ru>
2022-02-05 16:56:09 +0300

    Prepairing to deploy. Deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# initialize deployment
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+cd -
